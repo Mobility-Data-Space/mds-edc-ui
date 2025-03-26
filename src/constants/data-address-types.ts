edc58fc02435f85338d487d3b7be392b80382857
Moatez Mkhinini <moatezmkhinini@think-it.io>
2025-03-26 06:38:19 +0100

    feat: static data and translations for asset form

--- src/constants/data-address-types.ts
+++ src/constants/data-address-types.ts
@@ -1,10 +1,10 @@
-export const DATA_ADDRESS_SELECT_DATA = [
-    {
-        value: 'Http',
-        text: 'REST-API Endpoint',
-    },
-    {
-        value: 'Custom-Data-Address-Json',
-        text: `Custom Datasource-create Config (JSON)`,
-    },
-];
+export const DATA_ADDRESS_TYPE_HTTP = {
+    value: 'Http',
+    text: 'REST-API Endpoint',
+};
+export const DATA_ADDRESS_TYPE_CUSTOM = {
+    value: 'Custom-Data-Address-Json',
+    text: `Custom Datasource-create Config (JSON)`,
+}
+
+export const DATA_ADDRESS_SELECT_DATA = [ DATA_ADDRESS_TYPE_HTTP, DATA_ADDRESS_TYPE_CUSTOM ];
