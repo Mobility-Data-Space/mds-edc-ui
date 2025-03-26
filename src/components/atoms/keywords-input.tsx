c583a29f0b8ac1e989766f5ed4063821328bf639
Moatez Mkhinini <moatezmkhinini@think-it.io>
2025-03-26 06:29:27 +0100

    feat: material ui atoms

---
+++ src/components/atoms/keywords-input.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { IconButton, Tooltip } from "@mui/material";
+import { TextFieldProps } from "@mui/material/TextField/TextField";
+import { InfoOutlined } from '@mui/icons-material';
+import ChipInput from 'material-ui-chip-input'
+import FormControl from "@mui/material/FormControl";
+
+const ENTER = 13;
+const COMMA = 188;
+const SEMICOLON = 186;
+const chipKeyCodes = [ENTER, COMMA, SEMICOLON];
+
+export type KeywordsInputProps = TextFieldProps & {
+  error: boolean;
+  tooltip?: string,
+  label?: string,
+  placeholder?: string,
+  value: string[];
+  onChange: (value: string[]) => void;
+};
+
+export function KeywordsInput({ tooltip = "", label = "", placeholder = "", error, value, onChange, }: KeywordsInputProps): JSX.Element {
+  const onDelete= (chip: string, index: number) => {
+    value.splice(index, 1);
+    return onChange(value);
+  };
+
+  return (
+    <FormControl fullWidth>
+      <ChipInput
+        newChipKeyCodes={chipKeyCodes}
+        fullWidth
+        label={label}
+        InputLabelProps={label === "" ? { shrink: true } : {}}
+        variant={"outlined"}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        onDelete={onDelete}
+        error={error}
+        InputProps={{
+          endAdornment: <Tooltip title={tooltip}>
+            <IconButton classes={{ root: "!-mt-3.5" }}>
+              <InfoOutlined />
+            </IconButton>
+          </Tooltip>
+        }}
+      />
+    </FormControl>
+  );
+}
---
+++ src/components/molecules/key-value-pair-input-list.tsx
@@ -0,0 +1,116 @@
+import {TextFieldProps} from "@mui/material/TextField/TextField";
+import {KeyValuePairInput, Tag} from "@/components/atoms/key-value-pair-input.tsx";
+import {Button, FormHelperText} from "@mui/material";
+
+export type KeyValuePairInputListProps =  TextFieldProps & {
+  addText?: string;
+  removeText?: string;
+  errorText?: string;
+  helperText?: string;
+  keyLabel?: string;
+  keyPlaceholder?: string;
+  valueLabel?: string;
+  valuePlaceholder?: string;
+  valueOnly?: boolean;
+  additionalActions?: JSX.Element[];
+  value: TagsList;
+  onChange: (tagList: TagsList) => void;
+};
+
+type TagsList = { input: Tag; valid: boolean; id: string }[];
+
+const uid = function(){
+  return Date.now().toString(36) + Math.random().toString(36);
+}
+
+function tagKeyIsUnique(tagsList: TagsList, key: string, index: number) {
+  return !tagsList.find(
+    (tag, selectedTagIndex) =>
+      tag.input.key === key && selectedTagIndex !== index,
+  );
+}
+
+const withAddedTag = (tagsList: TagsList): TagsList => {
+  return [
+    ...tagsList,
+    { input: { key: "", value: "" }, valid: true, id: uid() },
+  ];
+};
+
+function withRemovedTag(tagsList: TagsList, tag: { input: Tag; valid: boolean; id: any }): TagsList {
+  return tagsList.filter(
+    (currentElement) => currentElement.id !== tag.id,
+  );
+}
+const withUpdatedTag = (
+  tagsList: TagsList,
+  { input, valid }: { input: Partial<Tag>; valid: boolean },
+  tag: any,
+  index: number,
+): TagsList => {
+  let updatedTag = {
+    input: { ...tag.input, ...input },
+    valid: false,
+    id: tag.id,
+  };
+
+  if (input.key && !tagKeyIsUnique(tagsList, input.key, index)) {
+    updatedTag = { ...updatedTag, valid: false };
+  } else {
+    updatedTag = { ...updatedTag, valid };
+  }
+
+  return tagsList.map((element, elementIndex) => {
+    if (index === elementIndex) {
+      return updatedTag;
+    } else return element;
+  });
+};
+
+export function KeyValuePairInputList({
+  addText = "Add",
+  removeText = "Remove",
+  errorText = "Please insert valid key/value pair",
+  helperText,
+  keyLabel = "Key",
+  keyPlaceholder = "Key",
+  valueLabel = "Value",
+  valuePlaceholder = "Value",
+  valueOnly = false,
+  additionalActions = [],
+  value,
+  onChange
+}: KeyValuePairInputListProps) {
+
+  return (
+    <div className="w-full flex flex-col gap-y-3">
+      {value.map((tagInput, index) => (
+        <KeyValuePairInput
+          keyLabel={keyLabel}
+          keyPlaceholder={keyPlaceholder}
+          valueLabel={valueLabel}
+          valuePlaceholder={valuePlaceholder}
+          key={tagInput.id}
+          onChange={({ input, valid }) => {
+            onChange(withUpdatedTag(value, { input, valid }, tagInput, index));
+          }}
+          value={tagInput.input}
+          onRemove={() => onChange(withRemovedTag(value, tagInput))}
+          removeText={removeText}
+          errorText={errorText}
+          valid={tagInput.valid}
+          valueOnly={valueOnly}
+        />
+      ))}
+
+      <FormHelperText>{helperText}</FormHelperText>
+
+      <div>
+        <Button onClick={() => onChange(withAddedTag(value))}>
+          {addText}
+        </Button>
+        {additionalActions}
+      </div>
+    </div>
+  );
+}
