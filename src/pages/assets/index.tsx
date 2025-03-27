3cbad90574a0e8b16e6212dfde5ef3b0ff0a8ed3
Moatez Mkhinini <moatezmkhinini@think-it.io>
2025-03-27 12:53:57 +0100

    refactor: add assets translations

--- src/pages/assets/index.tsx
+++ src/pages/assets/index.tsx
@@ -35,10 +35,10 @@ export default function AssetListPage() {
             <div className="flex items-center">
               <div className="flex-1">
                 <ConnectorDashboard.Title>
-                  <T string="title" />
+                  <T string="assets.title" />
                 </ConnectorDashboard.Title>
                 <ConnectorDashboard.Description>
-                  <T string="description" />
+                  <T string="assets.description" />
                 </ConnectorDashboard.Description>
               </div>
               <div>
@@ -47,35 +47,10 @@ export default function AssetListPage() {
                   onClick={() => setIsModalOpen(true)}
                 >
                   <Plus className="h-4 w-4" />
-                  <T string="buttonAdd" />
+                  <T string="assets.buttonAdd" />
                 </button>
               </div>
             </div>
-  {/*
-
-            <div className="sm:col-span-1">
-              <label
-                htmlFor="hs-as-table-product-review-search"
-                className="sr-only"
-              >
-                <T global string="search" />
-              </label>
-              <div className="relative flex rounded-lg">
-                <AssetsList.Search
-                  name="hs-as-table-product-review-search"
-                  className="py-3 px-4 ps-11 block w-full border-gray-200 shadow-sm rounded-s-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-                  placeholder={globalTranslator("searchPlaceholder")}
-                />
-                <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
-                  <Search className="w-4 h-4" />
-                </div>
-                <AssetsList.SearchTrigger className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-e-md border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
-                  <T global string="search" />
-                </AssetsList.SearchTrigger>
-              </div>
-            </div>
-
-  */}
           </ConnectorDashboard.Section>
 
           <Table className="table table-fixed">
