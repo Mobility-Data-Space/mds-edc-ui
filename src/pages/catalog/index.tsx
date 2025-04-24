c4aa611fcaa12fd6ecc94aed4d8c0b996b5ad291
Moatez Mkhinini <moatezmkhinini@think-it.io>
2025-04-24 02:04:36 +0100

    feat: created asset card and details modal

--- src/pages/catalog/index.tsx
+++ src/pages/catalog/index.tsx
@@ -1,45 +1,79 @@
+import { Button } from "@/components/atoms/button";
 import { Table } from "@/components/atoms/table";
-import { ConnectorStatus } from "@/components/organisms/connector-status";
 import { ConnectorDashboard } from "@/components/templates/connector-dashboard";
-import { DATASPACE } from "@/constants/dataspace";
+import { AssetsList } from "@think-it-labs/edc-connector-ui/assets-list";
 import { useConnectorDashboardState } from "@/hooks/use-connector-dashboard-state";
+import { usePagination } from "@/hooks/use-pagination";
 import { T } from "@/i18n";
+import { ChevronLeft, ChevronRight, PlusCircle } from "lucide-react";
+import {Modal, Box, Button as MuiButton, Dialog, DialogContent} from '@mui/material';
 
-export default function CatalogsPage() {
-  const { push } = useConnectorDashboardState();
+import React, {useState} from "react";
+import CreateAssetForm from "@/components/templates/create-asset-form.tsx";
+import SideDrawer from "@/components/organisms/side-drawer.tsx";
+import AssetCard from "@/components/organisms/asset-card.tsx";
+import {Catalog} from "@think-it-labs/edc-connector-ui/catalog.tsx";
+import {useListContext} from "@think-it-labs/edc-connector-ui/list";
+import {dataSetToAsset} from "@/schema/catalog.ts";
+import AssetDetails from "@/components/organisms/asset-details.tsx";
+import {Asset} from "@think-it-labs/edc-connector-client";
+
+export default function CatalogPage() {
+  const { push, connector } = useConnectorDashboardState();
+  const { page, decrementPage, incrementPage, offset, limit, hasPrev } = usePagination();
+  const [isDetailsModalOpen, setIsDetailsModalOpen] = useState(false);
+  const [openAsset, setOpenAsset] = useState({} as Asset);
+
+  const openDetailsModal = (asset: Asset) => {
+    setIsDetailsModalOpen(true);
+    setOpenAsset(asset);
+  };
 
   return (
-    <ConnectorDashboard>
-      <ConnectorDashboard.Section>
-        <ConnectorDashboard.Title>
-          <T string="title" />
-        </ConnectorDashboard.Title>
-        <ConnectorDashboard.Description>
-          <T string="description" />
-        </ConnectorDashboard.Description>
-      </ConnectorDashboard.Section>
+    <>
+      <Dialog
+        open={isDetailsModalOpen}
+        maxWidth="lg"
+        className="my-7"
+        onClose={() => setIsDetailsModalOpen(false)}
+      >
+        <DialogContent style={{ maxWidth: "90vw", width: "1000px" }}>
+          <AssetDetails asset={openAsset} />
+        </DialogContent>
+      </Dialog>
 
-      <Table className="table table-auto">
-        <Table.Head>
-          <Table.Row>
-            <Table.Heading className="w-16">
-              #
-            </Table.Heading>
+      <SideDrawer title={<T string="catalog.title" />}>
+        <Catalog managementUrl={connector.managementUrl} protocolUrl={connector.protocolUrl} >
+          <div className="flex justify-end py-4">
 
-            <Table.Heading>
-              <T string="headingName" />
-            </Table.Heading>
+            {/* TODO: move pagination here */}
+          </div>
 
-            <Table.Heading>
-              <T string="headingStatus" />
-            </Table.Heading>
-          </Table.Row>
-        </Table.Head>
+          <div className="flex flex-wrap gap-2.5">
+            <Catalog.Items
+              limit={limit}
+              offset={offset}
+              sortOrder="DESC"
+            >
+              {({ item, index }) => (
+                <AssetCard asset={dataSetToAsset(item) as any} key={index} onClick={() => openDetailsModal(dataSetToAsset(item) as any)} />
+              )}
+            </Catalog.Items>
+          </div>
 
-        <Table.Body>
-
-        </Table.Body>
-      </Table>
-    </ConnectorDashboard>
+          <Catalog.Loading>
+            <div className="max-w-20 mx-auto mt-4 flex flex-col bg-white border shadow-sm rounded-xl p-4 md:p-5">
+              <span
+                className="animate-spin mx-auto inline-block size-8 border-[3px] border-current border-t-transparent text-blue-600 rounded-full"
+                role="status"
+                aria-label="loading"
+              >
+                <span className="sr-only">Loading...</span>
+              </span>
+            </div>
+          </Catalog.Loading>
+        </Catalog>
+      </SideDrawer>
+    </>
   );
 }
---
+++ src/utilities/utilities.ts
@@ -0,0 +1,22 @@
+import {DELIMITER} from "@/i18n";
+
+export const truncate = (string: string, length: number = 10) => {
+  if (! string) {
+    return string;
+  }
+  return string.length > length ? string.substring(0, length) + "..." : string;
+}
+
+export const extractArrayValues = (array: any[]) => {
+  return array.map(item => {
+    try {
+      return item["https://w3id.org/edc/v0.0.1/ns/input"][0]["https://w3id.org/edc/v0.0.1/ns/value"][0]["@value"]
+    } catch (e) {
+      return "";
+    }
+  });
+}
+
+export const joinArrayValues = (array: any[]) => {
+  return extractArrayValues(array).join(DELIMITER);
+}
