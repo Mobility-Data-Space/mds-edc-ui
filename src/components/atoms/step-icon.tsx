d89e1113a76fcbb0f9f0a45531bc9dd1df88a02e
Moatez Mkhinini <moatezmkhinini@think-it.io>
2025-05-14 18:27:38 +0100

    feat: added pencil icon on completed steps in asset create form

---
+++ src/components/atoms/step-icon.tsx
@@ -0,0 +1,26 @@
+import {Chip, StepIcon as MuiStepIcon, StepIconProps} from "@mui/material";
+import React from "react";
+import {Create as CreateIcon} from "@mui/icons-material";
+
+export function StepIcon({ completed, active, icon, ...props }: StepIconProps): JSX.Element {
+  return (
+    <MuiStepIcon
+      {...props}
+      color="secondary"
+      icon={! completed ?
+        icon :
+        <Chip
+          label={<CreateIcon sx={{ fontSize: "14px", padding: 0 }} />}
+          size="small"
+          color="primary"
+          sx={{ "& .MuiChip-label": {
+            paddingLeft: "4px",
+            paddingRight: "4px",
+          } }}
+        />
+      }
+      active={active}
+      completed={completed}
+    />
+  );
+}
