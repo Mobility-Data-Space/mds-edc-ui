0f0f91a72c30cdd994ae2715baa949490a04c50c
Moatez Mkhinini <moatezmkhinini@think-it.io>
2025-06-04 01:36:08 +0100

    feat: feat: contract agreement state in show dialog

--- src/components/organisms/contract-agreement-dialog.tsx
+++ src/components/organisms/contract-agreement-dialog.tsx
@@ -1,10 +1,20 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { T } from "@/i18n";
-import {ContractAgreement} from "@think-it-labs/edc-connector-client";
-import {Button, Dialog, DialogActions, DialogContent, DialogTitle, Icon} from "@mui/material";
+import {Asset, ContractAgreement} from "@think-it-labs/edc-connector-client";
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle, Icon, LinearProgress} from "@mui/material";
 import {TitleWithIcon} from "@/components/atoms/TitleWithIcon";
 import {TransferFormDialog} from "@/components/templates/transfer-form-dialog";
 import ContractAgreementDetails from "@/components/organisms/contract-agreement-details";
+import ContractAgreementTerminateDialog from "@/components/organisms/contract-agreement-terminate-dialog";
+import {useEdcConnectorClient} from "@think-it-labs/edc-connector-ui/hooks/use-edc-connector-client";
+import {enqueueSnackbar} from "notistack";
+import {STATE_RUNNING, STATE_TERMINATED} from "@/schema/transfer-process";
+import {TransferProcess} from "@think-it-labs/edc-connector-client/dist/src/entities";
+import {TERMINATION_REASON_BY_USER} from "@/schema/contract-agreement";
+import Typography from "@mui/material/Typography";
+import {removeJsonLdSchemaFromProperties} from "@/utilities/catalog";
+import {readValue} from "@think-it-labs/edc-connector-ui/json-ld";
+import {Timestamp} from "@think-it-labs/edc-connector-ui/timestamp";
 
 interface ContractAgreementDialogProps {
   contractAgreement: ContractAgreement;
@@ -14,18 +24,66 @@ interface ContractAgreementDialogProps {
   managementUrl: string;
   contentStyle?: { [key: string]: string };
   translator: (key: string) => string;
-
+  retirementReason?: string;
 }
-export default function ContractAgreementDialog({ open, onClose, contractAgreement, participantId, managementUrl, contentStyle = {}, translator }: ContractAgreementDialogProps) {
+
+export default function ContractAgreementDialog({ open, onClose, contractAgreement, participantId, managementUrl, contentStyle = {}, translator, retirementReason = "" }: ContractAgreementDialogProps) {
   const [isTransferModalOpen, setIsTransferModalOpen] = useState(false);
 
-  const icon = <Icon fontSize="large">{contractAgreement.providerId === participantId ? "file_upload" : "file_download"}</Icon>;
+  const [asset, setAsset] = useState({} as Asset);
+  const [transferProcesses, setTransferProcesses] = useState<TransferProcess[]>([]);
+  const [isTerminated, setIsTerminated] = useState(false);
+  const [isTerminatedAt, setIsTerminatedAt] = useState<number>(0);
+  const [isInProgress, setIsInProgress] = useState(false);
+
+  const edcClient = useEdcConnectorClient({ management: managementUrl });
+  useEffect(() => {
+    if (! contractAgreement.assetId) {
+      return;
+    }
+    edcClient.management.assets.get(contractAgreement.assetId)
+    .then(setAsset)
+    .catch(error => enqueueSnackbar(translator('assets.[id].fetchError')))
+  }, [edcClient, contractAgreement.assetId]);
+
+  useEffect(() => {
+    if (! contractAgreement.id) {
+      return;
+    }
+    edcClient.management.transferProcesses.queryAll({
+      filterExpression: [{
+        "operandLeft": "contractId",
+        "operator": "=",
+        "operandRight": contractAgreement.id
+      }]
+    }).then((transferProcesses) => {
+      setTransferProcesses(transferProcesses);
+      setIsTerminated(false);
+      setIsInProgress(false);
+
+      let contractIsInProgress = false;
+      let contractIsTerminated = false;
+      let contractIsTerminatedAt = 0;
+      transferProcesses.forEach(transferProcess => {
+        if (transferProcess.state === STATE_TERMINATED) {
+          contractIsTerminated = true;
+          contractIsTerminatedAt = readValue(removeJsonLdSchemaFromProperties(transferProcess), "stateTimestamp");
+        } else if (transferProcess.state === STATE_RUNNING) {
+          contractIsInProgress = true;
+        }
+      });
+
+      setIsInProgress(! contractIsTerminated && contractIsInProgress);
+      setIsTerminated(contractIsTerminated);
+      setIsTerminatedAt(contractIsTerminatedAt);
+    });
+  }, [edcClient, contractAgreement.id]);
 
   return (
     <>
       <TransferFormDialog
         contractAgreementLd={contractAgreement}
-        isOpen={isTransferModalOpen}
+        open={isTransferModalOpen}
         onClose={() => setIsTransferModalOpen(false)}
         translator={translator}
       />
@@ -36,29 +94,71 @@ export default function ContractAgreementDialog({ open, onClose, contractAgreeme
         onClose={onClose}
       >
         <DialogTitle>
-          <TitleWithIcon icon={icon} title={contractAgreement.assetId} subtitle={participantId} />
+          <TitleWithIcon
+            icon={<Icon fontSize="large" color={isTerminated ? "error" : "inherit"}>
+              {
+                (contractAgreement.consumerId === participantId ? "file_download" : "file_upload") +
+                (isTerminated ? "_off" : "")
+              }
+            </Icon>}
+            title={contractAgreement.assetId}
+            subtitle={participantId}
+          />
         </DialogTitle>
         <DialogContent style={contentStyle}>
-          <ContractAgreementDetails contractAgreement={contractAgreement} participantId={participantId} managementUrl={managementUrl} />
+          <div className="flex flex-col gapy-y-3">
+            {isInProgress && <LinearProgress className="my-3" />}
+
+            {isTerminated &&
+              <div className="flex gap-x-3 p-4 mb-3 rounded bg-red-50">
+                <svg viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="h-5 w-5 mt-1 text-red-400">
+                  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z" clip-rule="evenodd" />
+                </svg>
+                <div>
+                  <Typography variant="subtitle1" className="text-red-800">
+                    {TERMINATION_REASON_BY_USER}
+                  </Typography>
+                  <Typography variant="body2" className="text-red-800">
+                    {retirementReason}
+                  </Typography>
+                  <Typography variant="body2" className="text-red-800">
+                    <Timestamp seconds={isTerminatedAt} year="numeric" month="2-digit" day="2-digit" hour="numeric" minute="numeric" />
+                  </Typography>
+                </div>
+              </div>
+            }
+
+            <ContractAgreementDetails
+              contractAgreement={contractAgreement}
+              participantId={participantId}
+              asset={asset}
+              transferProcesses={transferProcesses}
+            />
+          </div>
         </DialogContent>
         <DialogActions>
-          <Button
-            data-testid="transfer-process-terminate"
-            variant="contained"
-            onClick={() => ""}
-          >
-            <T string="common.terminate"/>
-          </Button>
-          <Button color="secondary" onClick={onClose}>
-            <T string="common.close"/>
-          </Button>
-          <Button
-            data-testid="transfer-process-submit"
-            variant="contained"
-            onClick={() => setIsTransferModalOpen(true)}
-          >
-            <T string="common.transfer"/>
-          </Button>
+          <div className="flex justify-between flex-grow p-3">
+            {!isTerminated && <Button
+              data-testid="transfer-process-terminate"
+              variant="contained"
+              color="error"
+              onClick={() => setIsTerminateModalOpen(true)}
+            >
+              <T string="common.terminate"/>
+            </Button>}
+            <div className="flex flex-grow justify-end gap-x-3">
+              <Button color="secondary" onClick={onClose} className="self-end">
+                <T string="common.close"/>
+              </Button>
+              {!isTerminated && <Button
+                data-testid="transfer-process-submit"
+                variant="contained"
+                onClick={() => setIsTransferModalOpen(true)}
+              >
+                <T string="common.transfer"/>
+              </Button>}
+            </div>
+          </div>
         </DialogActions>
       </Dialog>
     </>
