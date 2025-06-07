7f9fd543d4589db23163acd091157313c97c8db8
Moatez Mkhinini <moatezmkhinini@think-it.io>
2025-06-08 00:48:18 +0100

    feat: added s3 and azure data address types in transfer form dialog

--- src/components/templates/transfer-form-dialog.tsx
+++ src/components/templates/transfer-form-dialog.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import {enqueueSnackbar} from "notistack";
 import {Dialog, DialogActions, DialogContent, DialogTitle} from "@mui/material";
 import Typography from "@mui/material/Typography";
@@ -14,6 +14,8 @@ import {useParticipantConnectorState} from "@/hooks/use-participant-connector-st
 import {removeJsonLdSchemaFromProperties} from "@/utilities/catalog";
 import { DataAddressTypes, defaultHttpDataAddress } from "@/utilities/data-address";
 import {createTransferProcessRequest} from "@/utilities/transfer-process";
+import {AssetContactEmailAndSubject} from "@/components/molecules/asset-contact-email-and-subject.tsx";
+import {AssetFormDataAddressAmazonS3} from "@/components/organisms/asset-form-data-address-amazon-s3.tsx";
 
 export interface TransferFormDialogProps {
   open: boolean,
@@ -28,7 +30,8 @@ export function TransferFormDialog({ contractAgreementLd, open, onClose, transla
   const [errors, setErrors] = useState<DataAddress>({} as DataAddress);
   const contractAgreement = removeJsonLdSchemaFromProperties(contractAgreementLd);
   const { connector } = useParticipantConnectorState();
-  const edcClient = useEdcConnectorClient({management: connector.managementUrl}) ;
+  const edcClient = useEdcConnectorClient({management: connector.managementUrl});
+  const formRef = useRef<HTMLFormElement>(null);
 
   const onSubmit = () => {
     const agreement: Partial<ContractAgreement> = {
@@ -56,82 +59,117 @@ export function TransferFormDialog({ contractAgreementLd, open, onClose, transla
         </Typography>
       </DialogTitle>
       <DialogContent style={{ maxWidth: "80vw", width: "800px" }}>
-        <div className="flex flex-col gap-y-5">
-          <div className="flex flex-col gap-y-5 items-start">
-            <label
-              htmlFor="data-address-type"
-              className="inline-block text-sm text-black font-medium mb-2"
-            >
-              <T string="assets.new.fieldDataAddressType"/>
-            </label>
-            <MuiSelect
-              name="data-address-type"
-              id="data-address-type"
-              label={translator("assets.new.fieldDataAddressType")}
-              options={DATA_ADDRESS_SELECT_DATA}
-              error={errors["TRANSFER_PROCESS_DATA_ADDRESS_TYPE"]}
-              value={formData.type}
-              onChange={(event) => setFormData({...formData, type: event.target.value})}
-            />
-          </div>
+        <form
+          ref={formRef}
+          onSubmit={(event) => {
+            event.preventDefault();
+            return onSubmit();
+          }}
+        >
+          <div className="flex flex-col gap-y-5">
+            <div className="flex flex-col gap-y-5 items-start">
+              <label
+                htmlFor="data-address-type"
+                className="inline-block text-sm text-black font-medium mb-2"
+              >
+                <T string="assets.new.fieldDataAddressType"/>
+              </label>
+              <MuiSelect
+                name="data-address-type"
+                id="data-address-type"
+                label={translator("assets.new.fieldDataAddressType")}
+                options={DATA_ADDRESS_SELECT_DATA}
+                error={errors["TRANSFER_PROCESS_DATA_ADDRESS_TYPE"]}
+                value={formData.type}
+                onChange={(event) => setFormData({...formData, type: event.target.value})}
+              />
+            </div>
 
-          {formData.type == DataAddressTypes.CustomJson &&
-            <Input
-              name="properties-description"
-              id="properties-description"
-              key="properties-description"
-              multiline
-              rows={2}
-              label={DATA_ADDRESS_SELECT_DATA.find(option => option.value === formData.type)?.text}
-              placeholder={'{}'}
-              required
-              helperText={typeof errors["TRANSFER_PROCESS_DATA_DESTINATION"] === "string" ? errors["TRANSFER_PROCESS_DATA_DESTINATION"] : ""}
-              classes={{textField: {'& p': {color: theme.palette.error.main}}} as any}
-              error={errors["TRANSFER_PROCESS_DATA_DESTINATION"]}
-              value={formData.description}
-              onChange={(event) => setFormData({...formData, description: event.target.value})}
-            />
-          }
-          {formData.type === DataAddressTypes.HttpData &&
-            <>
-              <div className="flex gap-x-3">
-                <MuiSelect
-                  name="data-address-method"
-                  id="data-address-method"
-                  label={translator("assets.new.fieldDataAddressMethod")}
-                  options={[
-                    {value: "POST"},
-                    {value: "PUT"},
-                    {value: "PATCH"},
-                    {value: "DELETE"},
-                    {value: "OPTIONS"},
-                  ]}
-                  error={errors["TRANSFER_PROCESS_HTTP_METHOD"]}
-                  value={formData.method}
-                  onChange={(event) => setFormData({...formData, method: event.target.value})}
-                />
-                <Input
-                  name="data-address-base-url"
-                  id="data-address-base-url"
-                  data-testid="data-address-base-url"
-                  type="url"
-                  required
-                  placeholder={"https://"}
-                  label={translator("assets.new.fieldDataAddressUrl")}
-                  error={errors["TRANSFER_PROCESS_HTTP_URL"]}
-                  value={formData.baseUrl}
-                  onChange={(event) => setFormData({...formData, baseUrl: event.target.value})}
-                />
-              </div>
-            </>
-          }
-        </div>
+            {formData.type == DataAddressTypes.CustomJson &&
+              <Input
+                name="properties-description"
+                id="properties-description"
+                key="properties-description"
+                multiline
+                rows={2}
+                label={DATA_ADDRESS_SELECT_DATA.find(option => option.value === formData.type)?.text}
+                placeholder={'{}'}
+                required
+                helperText={typeof errors["TRANSFER_PROCESS_DATA_DESTINATION"] === "string" ? errors["TRANSFER_PROCESS_DATA_DESTINATION"] : ""}
+                classes={{textField: {'& p': {color: theme.palette.error.main}}} as any}
+                error={errors["TRANSFER_PROCESS_DATA_DESTINATION"]}
+                value={formData.description}
+                onChange={(event) => setFormData({...formData, description: event.target.value})}
+              />
+            }
+            {formData.type === DataAddressTypes.HttpData &&
+              <>
+                <div className="flex gap-x-3">
+                  <MuiSelect
+                    name="data-address-method"
+                    id="data-address-method"
+                    label={translator("assets.new.fieldDataAddressMethod")}
+                    options={[
+                      {value: "POST"},
+                      {value: "PUT"},
+                      {value: "PATCH"},
+                      {value: "DELETE"},
+                      {value: "OPTIONS"},
+                    ]}
+                    error={errors["TRANSFER_PROCESS_HTTP_METHOD"]}
+                    value={formData.method}
+                    onChange={(event) => setFormData({...formData, method: event.target.value})}
+                  />
+                  <Input
+                    name="data-address-base-url"
+                    id="data-address-base-url"
+                    data-testid="data-address-base-url"
+                    type="url"
+                    required
+                    placeholder={"https://"}
+                    label={translator("assets.new.fieldDataAddressUrl")}
+                    error={errors["TRANSFER_PROCESS_HTTP_URL"]}
+                    value={formData.baseUrl}
+                    onChange={(event) => setFormData({...formData, baseUrl: event.target.value})}
+                  />
+                </div>
+              </>
+            }
+
+            {formData.type === DataAddressTypes.MDSOnRequestOffer &&
+              <AssetContactEmailAndSubject
+                translator={translator}
+                formData={formData}
+                onChange={setFormData}
+                errors={errors}
+              />
+            }
+
+            {formData.type === DataAddressTypes.AmazonS3 &&
+              <AssetFormDataAddressAmazonS3
+                translator={translator}
+                formData={formData}
+                onChange={setFormData}
+                errors={errors}
+              />
+            }
+
+            {formData.type === DataAddressTypes.AzureBlob &&
+              <AssetFormDataAddressAmazonS3
+                translator={translator}
+                formData={formData}
+                onChange={setFormData}
+                errors={errors}
+              />
+            }
+          </div>
+        </form>
       </DialogContent>
       <DialogActions>
         <Button color="secondary" onClick={onClose}>
           <T string="common.close"/>
         </Button>
-        <Button color="primary" variant="contained" onClick={onSubmit}>
+        <Button color="primary" variant="contained" onClick={() => formRef.current && formRef.current.requestSubmit()}>
           <T string="transferProcesses.new.initiateTransfer"/>
         </Button>
       </DialogActions>
