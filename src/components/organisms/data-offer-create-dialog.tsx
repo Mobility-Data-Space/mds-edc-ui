14436ff91e7606873d0e9105d7cdfd524e21f8b8
Moatez Mkhinini <moatezmkhinini@think-it.io>
2025-06-08 00:19:29 +0100

    feat: moved publish data offer from a separate page to a modal

---
+++ src/components/organisms/data-offer-create-dialog.tsx
@@ -0,0 +1,203 @@
+import React, {useEffect, useRef, useState} from "react";
+import { enqueueSnackbar } from "notistack";
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle, Icon, Typography} from "@mui/material";
+import { useEdcConnectorClient } from "@think-it-labs/edc-connector-ui/hooks/use-edc-connector-client";
+import { ContractDefinitionFormWrapper } from "@think-it-labs/edc-connector-ui/contract-definition-form-wrapper";
+import { useParticipantConnectorState} from "@/hooks/use-participant-connector-state";
+import { T, useTranslator } from "@/i18n";
+import { fromContractDefinitionForm, MdsContractDefinitionInput } from "@/utilities/contract-definition";
+import { defaultCreateContractDefinitionFormData } from "@/utilities/contract-definition";
+import {MuiSelect} from "@/components/atoms/mui-select";
+import { Input } from "@/components/atoms/input";
+import {Checkbox} from "@/components/atoms/checkbox";
+import {idMultipleReader, idMultipleSelector} from "@/utilities/data-offer";
+import {Asset} from "@think-it-labs/edc-connector-client";
+import AssetDialog from "@/components/organisms/asset-dialog.tsx";
+
+interface DataOfferCreateDialogProps {
+  open: boolean;
+  onClose: () => void;
+  managementUrl: string;
+  connectorEndpoint: string;
+  participantId: string;
+  translator: (key: string) => string;
+}
+
+const optionsGenerator = (data: { "@id": string }[]) => {
+  return data.map(entry => ({
+    value: entry["@id"]
+  }));
+};
+
+export default function DataOfferCreateDialog({ open, onClose, managementUrl, connectorEndpoint, participantId, translator }: DataOfferCreateDialogProps) {
+  const submitButtonRef = useRef<HTMLButtonElement>(null);
+
+  const [assetDialogIsOpen, setAssetDialogIsOpen] = useState(false);
+  const [clickedAsset, setClickedAsset] = useState({} as Asset);
+  const [assetsById, setAssetsById] = useState<{ [key: string]: Asset }>({});
+  const [assetIds, setAssetIds] = useState<{ value: string }[]>([]);
+  const [policyIds, setPolicyIds] = useState<{ value: string }[]>([]);
+
+  const edcClient = useEdcConnectorClient({management: managementUrl});
+
+  useEffect(() => {
+    edcClient.management.assets.queryAll({ offset: 0 })
+    .then(result => {
+      setAssetIds(optionsGenerator(result));
+      const assets: { [key: string]: Asset } = {};
+      result.forEach(asset => {
+        assets[asset.id] = asset;
+      });
+      setAssetsById(assets);
+    })
+    .catch(error => {
+      setAssetIds([])
+      setAssetsById({});
+    });
+
+    edcClient.management.policyDefinitions.queryAll({ offset: 0 })
+    .then(result => setPolicyIds(optionsGenerator(result)))
+    .catch(error => setPolicyIds([]));
+  }, [edcClient]);
+
+  const [formData, setFormData] = useState<MdsContractDefinitionInput>(defaultCreateContractDefinitionFormData);
+  const validateForm = () => true ;
+
+
+  const onChange = (newFormData: MdsContractDefinitionInput) => {
+    setFormData({ ...newFormData });
+  }
+  const onSubmit = () => {
+    if (!validateForm()) {
+      return;
+    }
+
+    if (submitButtonRef.current && submitButtonRef.current.form) {
+      submitButtonRef.current.form.requestSubmit();
+    }
+  };
+
+  const onFormSubmitFail = (error: Error) => {
+    const match = /"message":"(.*?)"/.exec(error.message)
+    enqueueSnackbar((match && match[1]) || translator("policyDefinition.new.saveFail"));
+  };
+
+  return (
+    <>
+      <AssetDialog
+        asset={clickedAsset}
+        participantId={participantId}
+        connectorEndpoint={connectorEndpoint}
+        open={assetDialogIsOpen}
+        onClose={() => setAssetDialogIsOpen(false)}
+      />
+      <Dialog
+        open={open}
+        maxWidth="lg"
+        className="my-7"
+        onClose={onClose}
+      >
+        <ContractDefinitionFormWrapper
+          managementUrl={managementUrl}
+          formData={() => fromContractDefinitionForm(formData)}
+          onSuccess={onClose}
+          onFailure={onFormSubmitFail}
+        >
+          <DialogTitle>
+            <Typography variant="h5" >
+              <T string="contractDefinitions.new.publishNewDataOffer" />
+            </Typography>
+          </DialogTitle>
+          <DialogContent style={{ maxWidth: "80vw", width: "800px" }}>
+            <div className="flex flex-col pt-5 gap-y-5">
+              <div>
+                <Input
+                  required
+                  name="contract-definition-id"
+                  id="contract-definition-id"
+                  data-testid="contract-definition-id"
+                  label={translator("contractDefinitions.new.id")}
+                  value={formData["@id"]}
+                  onChange={(event) => onChange({...formData, ["@id"]: event.target.value})}
+                />
+              </div>
+              <MuiSelect
+                multiple
+                required
+                name="assets-selector"
+                id="asset-id"
+                data-testid="asset-id"
+                label={translator("contractDefinitions.new.assets")}
+                options={assetIds}
+                value={idMultipleReader(formData.assetsSelector)}
+                onChange={(event) => onChange({...formData, assetsSelector: idMultipleSelector(event.target.value)})}
+              />
+
+              <MuiSelect
+                required
+                name="contract-policy-id"
+                id="contract-policy-id"
+                data-testid="contract-policy-id"
+                label={translator("contractDefinitions.new.contractPolicy")}
+                options={policyIds}
+                value={formData.contractPolicyId}
+                onChange={(event) => onChange({...formData, contractPolicyId: event.target.value})}
+              />
+
+              <MuiSelect
+                required
+                name="access-policy-id"
+                id="access-policy-id"
+                data-testid="access-policy-id"
+                label={translator("contractDefinitions.new.accessPolicy")}
+                options={policyIds}
+                value={formData.accessPolicyId}
+                onChange={(event) => onChange({...formData, accessPolicyId: event.target.value})}
+              />
+
+              <Checkbox
+                label={translator("contractDefinitions.new.manualApproval")}
+                value={formData.privateProperties.manualApproval}
+                onChange={(event) => {
+                  onChange({...formData, privateProperties: {manualApproval: event.target.checked}})
+                }}
+              />
+
+              <div className="flex gap-4">
+                {idMultipleReader(formData.assetsSelector).map(assetId => (
+                  <div key={assetId} className="p-4 rounded shadow-sm" onClick={() => {
+                    setClickedAsset(assetsById[assetId]);
+                    setAssetDialogIsOpen(true);
+                  }}>
+                    <Typography component="a" className="hover:underline cursor-pointer">
+                      {assetId}
+                    </Typography>
+                  </div>
+                ))}
+              </div>
+
+            </div>
+          </DialogContent>
+          <DialogActions>
+            <div className="flex flex-row self-end gap-x-5 pr-3 pb-3">
+              <Button
+                color="secondary"
+                onClick={onClose}
+              >
+                <T string="common.cancel"/>
+              </Button>
+              <Button
+                ref={submitButtonRef}
+                variant="contained"
+                color="primary"
+                onClick={onSubmit}
+              >
+                <T string="common.create"/>
+              </Button>
+            </div>
+          </DialogActions>
+        </ContractDefinitionFormWrapper>
+      </Dialog>
+    </>
+  );
+}
