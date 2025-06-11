251e63b811567b2dd1bd3d42b2c26aaf13b9e295
Moatez Mkhinini <moatezmkhinini@think-it.io>
2025-06-11 02:02:09 +0100

    feat: date format helper

--- src/utilities/utilities.ts
+++ src/utilities/utilities.ts
@@ -1,4 +1,9 @@
 import {DELIMITER} from "@/i18n";
+import TimeAgo from "javascript-time-ago";
+import {TIME_LOCALE} from "@/constants/time-locale.ts";
+import en from 'javascript-time-ago/locale/en'
+
+TimeAgo.addDefaultLocale(en)
 
 export const truncate = (string: string, length: number = 10) => {
   if (! string) {
@@ -42,15 +47,32 @@ export const tryTranslatingWithTooltip = (value: string, prefix: string, transla
   return [tooltipTitle, computedValue];
 }
 
-export const formatDate = (melliSecondsTimestamp: number) => {
-  const formatter = new Intl.DateTimeFormat("en-GB", {
+export interface FormatDateTimeOptions {
+  showSeconds?: boolean,
+  showDayOfWeek?: boolean,
+}
+
+export const formatDateTime = (melliSecondsTimestamp: number, { showDayOfWeek, showSeconds }: FormatDateTimeOptions = { showSeconds: false, showDayOfWeek: false }) => {
+  const formatter = new Intl.DateTimeFormat(TIME_LOCALE, {
     timeZone: "Europe/Berlin",
     year: "numeric",
     month: "2-digit",
     day: "2-digit",
     hour: "numeric",
     minute: "numeric",
+    ...(showSeconds ? { second: "numeric" } : {}),
+    ...(showDayOfWeek ? { weekday: "long" } : {}),
   });
 
   return formatter.format(melliSecondsTimestamp);
+}
+
+export const formatDateTimeAgo = (melliSecondsTimestamp: number) => {
+  const timeAgo = new TimeAgo(TIME_LOCALE);
+
+  if (! melliSecondsTimestamp) {
+    return "";
+  }
+
+  return timeAgo.format(new Date(melliSecondsTimestamp));
 }
