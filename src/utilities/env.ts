409b0697cb698131a0a1abf10eab6a06245301b0
Moatez Mkhinini <moatezmkhinini@think-it.io>
2025-06-19 00:54:51 +0100

    feat: added proxy to append api key

--- src/utilities/env.ts
+++ src/utilities/env.ts
@@ -1,11 +1,18 @@
 import {Participant} from "@/constants/dataspace.ts";
 
+export const managementPrefix = "/api/connector/management";
+
+export function connectorApiKey() {
+  return process.env.EDC_MANAGEMENT_API_KEY || "";
+}
+
 export function participantConfig() {
   return {
     id: process.env.EDC_ID || "",
     name: process.env.EDC_NAME || "",
     edcUrl: process.env.EDC_URL || "",
-    managementUrl: process.env.EDC_MANAGEMENT_URL || "",
+    managementUrl: managementPrefix,
+    connectorManagementUrl: process.env.EDC_MANAGEMENT_URL || "",
     defaultUrl: process.env.EDC_DEFAULT_URL || "",
     protocolUrl: process.env.EDC_PROTOCOL_URL || "",
   };
