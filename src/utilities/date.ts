65a89bcf9ab8149ac4e561349bc1a41f9e0b62c3
Moatez Mkhinini <moatezmkhinini@think-it.io>
2025-07-02 03:17:48 +0100

    refactor: added missing import

--- src/utilities/date.ts
+++ src/utilities/date.ts
@@ -1,6 +1,7 @@
 import dayjs, {Dayjs} from "dayjs";
 import TimeAgo from "javascript-time-ago";
 import en from 'javascript-time-ago/locale/en'
+import {TIME_LOCALE} from "@/constants/time-locale.ts";
 
 TimeAgo.addDefaultLocale(en);
 
