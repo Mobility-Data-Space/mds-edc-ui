8033bf78f815925ab594a6291cbbc568aba8b79c
Ramy Hcini <ramyhcini@think-it.io>
2025-07-02 12:30:25 +0100

    ci: use ip4 instead of localhost in urls

--- tests/utils/ui-config.ts
+++ tests/utils/ui-config.ts
@@ -1,8 +1,8 @@
 export const config = {
   EDC_ID: "MDSXXXXX.YYYYY",
   EDC_NAME: "My MDS EDC Connector",
-  EDC_MANAGEMENT_URL: "http://localhost:8182/api/management",
-  EDC_DEFAULT_URL: "http://localhost:8181/api",
-  EDC_PROTOCOL_URL: "http://localhost:8183/api/dsp",
-  EDC_PUBLIC_URL: "http://localhost:8185/public"
+  EDC_MANAGEMENT_URL: "http://127.0.0.1:8182/api/management",
+  EDC_DEFAULT_URL: "http://127.0.0.1:8181/api",
+  EDC_PROTOCOL_URL: "http://127.0.0.1:8183/api/dsp",
+  EDC_PUBLIC_URL: "http://127.0.0.1:8185/public"
 }
