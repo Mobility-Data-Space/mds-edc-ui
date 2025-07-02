72c8190fc3e27090c95a01d223b9194b04e73494
Ramy Hcini <ramyhcini@think-it.io>
2025-07-02 12:11:54 +0100

    ci: use ip4 instead of localhost in urls

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   reporter: 'html',
   use: {
     // Base URL to use in actions like `await page.goto('/')`.
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://127.0.0.1:3000',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'on-first-retry'
@@ -45,7 +45,7 @@ export default defineConfig({
   // Run your local dev server before starting the tests.
   webServer: [{
       command: 'yarn dev',
-      url: 'http://localhost:3000',
+      url: 'http://127.0.0.1:3000',
       env: UiConfig,
       reuseExistingServer: !process.env.CI,
     },
