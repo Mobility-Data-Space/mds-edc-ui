6b9c22aff0c33a8444dca56c1d05b3cdee4df5fb
Ramy Hcini <ramyhcini@think-it.io>
2025-07-02 17:09:37 +0100

    chore: uncomment browser platforms for testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    /*{
+    {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
@@ -39,7 +39,7 @@ export default defineConfig({
     {
       name: 'Mobile Safari',
       use: { ...devices['iPhone 12'] },
-    }*/
+    }
   ],
 
   // Run your local dev server before starting the tests.
