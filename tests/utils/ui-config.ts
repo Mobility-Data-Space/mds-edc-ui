074bfde29f4cbc8980d8c77a0f6e57a5c1e26643
Ramy Hcini <ramyhcini@think-it.io>
2025-07-02 15:28:04 +0100

    ci: add API KEY to the config for testing

--- tests/utils/ui-config.ts
+++ tests/utils/ui-config.ts
@@ -4,5 +4,6 @@ export const config = {
   EDC_MANAGEMENT_URL: "http://127.0.0.1:8182/api/management",
   EDC_DEFAULT_URL: "http://127.0.0.1:8181/api",
   EDC_PROTOCOL_URL: "http://127.0.0.1:8183/api/dsp",
-  EDC_PUBLIC_URL: "http://127.0.0.1:8185/public"
+  EDC_PUBLIC_URL: "http://127.0.0.1:8185/public",
+  EDC_MANAGEMENT_API_KEY: "MyAPIKey"
 }
