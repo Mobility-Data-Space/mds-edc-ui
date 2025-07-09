b4bbc84cfe8f7846599e842e485c2b1c1a1c4d34
Moatez Mkhinini <moatezmkhinini@think-it.io>
2025-07-09 03:03:08 +0100

    feat: added proxy components

--- src/components/organisms/asset-form-data-address-http.tsx
+++ src/components/organisms/asset-form-data-address-http.tsx
@@ -5,6 +5,10 @@ import {T} from "@/i18n";
 import {Input} from "@/components/atoms/input";
 import {MuiSelect} from "@/components/atoms/mui-select";
 import {RadioButton} from "@/components/atoms/radio-button";
+import {DataAddressProxyQueryParams} from "@/components/molecules/data-address-proxy-query-params.tsx";
+import {DataAddressAuthHeaders} from "@/components/molecules/data-address-auth-headers.tsx";
+import {DataAddressHttpHeaders} from "@/components/molecules/data-address-http-headers.tsx";
+import {theme} from "@/theme/ThemeProvider.tsx";
 
 export interface AssetFormDataAddressHttpProps {
   translator: (key: string) => string,
@@ -13,91 +17,162 @@ export interface AssetFormDataAddressHttpProps {
   errors: { [key: string]: boolean | string },
   methodAlwaysShowing?: boolean,
   isPull?: boolean,
+  isDestination?: boolean,
 }
 
-export function AssetFormDataAddressHttp({ formData, errors, onChange, translator, methodAlwaysShowing = false, isPull = false }: AssetFormDataAddressHttpProps): JSX.Element {
+const sourceMethods = [ "GET", "POST" ].map((value) => ({ value }));
+const destinationMethods = [ "POST", "PUT", "PATCH", "DELETE" ].map((value) => ({ value }));
+const proxyMethods = [ "", "GET", "POST", "PUT", "PATCH", "DELETE" ].map((value) => ({ value }));
+
+export function AssetFormDataAddressHttp({ formData, errors, onChange, translator, methodAlwaysShowing = false, isPull = false, isDestination = false }: AssetFormDataAddressHttpProps): JSX.Element {
   if (isPull) {
     return <></>;
   }
 
+  const methods = isDestination ? destinationMethods : sourceMethods;
+
   return (
     <>
       <div className="flex flex-col gap-y-5">
-        <label
-          htmlFor="data-address-method"
-          className="inline-block text-sm text-black font-medium mb-2"
-        >
-          <T string="assets.new.fieldDataAddressMethodAndContentType"/>
-        </label>
-        <div>
-          {methodAlwaysShowing || !JSON.parse(formData.proxyMethod || "false") ?
-            <MuiSelect
-              name="data-address-method"
-              id="data-address-method"
-              label={translator("assets.new.fieldDataAddressMethod")}
-              options={[
-                {value: "GET"},
-                {value: "POST"},
-              ]}
-              error={errors.method}
-              value={formData.method}
-              disabled={methodAlwaysShowing && !! JSON.parse(formData.proxyMethod || "false")}
-              onChange={(event) => onChange({ ...formData, method: event.target.value })}
-            />
-            : ""
-          }
-          {methodAlwaysShowing || JSON.parse(formData.proxyMethod || "false") ?
-            <FormHelperText>
-              <T string="assets.new.fieldDataAddressHttpProxyMethodHelper" />
-            </FormHelperText>
-            : ""
-          }
-        </div>
+        {isDestination ? "" :
+          <label
+            htmlFor="data-address-method"
+            className="inline-block text-sm text-black font-medium mb-2"
+          >
+            <T string="assets.new.fieldDataAddressMethodAndContentType"/>
+          </label>
+        }
+        <div className={isDestination ? 'sm:grid sm:grid-cols-3 gap-x-3' : ''}>
+          <div className="sm:col-span-1">
+            {methodAlwaysShowing || !JSON.parse(formData.proxyMethod || "false") ?
+              <MuiSelect
+                name="data-address-method"
+                id="data-address-method"
+                label={translator("assets.new.fieldDataAddressMethod")}
+                options={methods}
+                error={errors.method}
+                value={formData.method}
+                disabled={methodAlwaysShowing}
+                onChange={(event) => onChange({ ...formData, method: event.target.value })}
+              />
+              : ""
+            }
+            {methodAlwaysShowing ?
+              <FormHelperText>
+                <T string="assets.new.fieldDataAddressHttpProxyMethodHelper" />
+              </FormHelperText>
+              : ""
+            }
+          </div>
+          <div className="sm:col-span-2 flex flex-col gap-y-5">
+            {isDestination ? "" :
+              <label
+                htmlFor="data-address-base-url"
+                className="inline-block text-sm font-medium text-gray-800 mt-2.5"
+              >
+                <T string="assets.new.fieldDataAddressUrl"/>
+              </label>
+            }
+            {formData.baseUrl && !isDestination &&
+              <FormHelperText>
+                {translator("assets.new.fieldDataAddressHttpProxyPathHelper")}
+              </FormHelperText>
+            }
 
-        <div>
-          <RadioButton
-            id="data-address-http-proxy-method"
-            labelTrue={translator("assets.new.fieldDataAddressHttpProxyMethodTrue")}
-            labelFalse={translator("assets.new.fieldDataAddressHttpProxyMethodFalse")}
-            value={formData.proxyMethod}
-            onChange={(value) => onChange({ ...formData, proxyMethod: value })}
-          />
+            <Input
+              name="data-address-base-url"
+              id="data-address-base-url"
+              data-testid="data-address-base-url"
+              type="url"
+              required
+              placeholder={"https://"}
+              label={translator("assets.new.fieldDataAddressUrl")}
+              error={errors.baseUrl}
+              value={formData.baseUrl}
+              onChange={(event) => onChange({...formData, baseUrl: event.target.value})}
+            />
+          </div>
         </div>
       </div>
 
-      <div className="flex flex-col gap-y-5 items-start">
-        <label
-          htmlFor="data-address-base-url"
-          className="inline-block text-sm text-gray-800 mt-2.5"
-        >
-          <T string="assets.new.fieldDataAddressUrl"/>
-        </label>
-        {formData.baseUrl &&
-          <FormHelperText>
-            {translator("assets.new.fieldDataAddressHttpProxyPathHelper")}
-          </FormHelperText>
-        }
+
+
+      {isDestination && <div className="flex flex-col gap-y-5 items-start">
+        <DataAddressProxyQueryParams
+          translator={translator}
+          formData={formData}
+          onChange={onChange}
+          errors={errors}
+        />
+
+        <DataAddressAuthHeaders
+          translator={translator}
+          formData={formData}
+          onChange={onChange}
+          errors={errors}
+        />
+
+        <DataAddressHttpHeaders
+          translator={translator}
+          formData={formData}
+          onChange={onChange}
+          errors={errors}
+        />
+
+        <div className="sm:grid sm:grid-cols-3 gap-x-3 w-full">
+          <div className="sm:col-span-1">
+            <MuiSelect
+              name="data-address-proxy-method"
+              id="data-address-proxy-method"
+              label={translator("assets.new.fieldDataAddressProxyMethod")}
+              options={proxyMethods}
+              error={errors.proxyMethod}
+              value={formData.proxyMethod}
+              onChange={(event) => onChange({...formData, proxyMethod: event.target.value})}
+            />
+          </div>
+          <div className="sm:col-span-2">
+            <Input
+              name="data-address-proxy-path"
+              id="data-address-proxy-path"
+              data-testid="data-address-proxy-path"
+              required
+              placeholder="sub-path/endpoint"
+              label={translator("assets.new.fieldDataAddressProxyPath")}
+              error={errors.proxyPath}
+              value={formData.proxyPath}
+              onChange={(event) => onChange({...formData, proxyPath: event.target.value})}
+            />
+          </div>
+        </div>
 
         <Input
-          name="data-address-base-url"
-          id="data-address-base-url"
-          data-testid="data-address-base-url"
-          type="url"
+          name="data-address-content-type"
+          id="data-address-content-type"
+          data-testid="data-address-content-type"
           required
-          placeholder={"https://"}
-          label={translator("assets.new.fieldDataAddressUrl")}
-          error={errors.baseUrl}
-          value={formData.baseUrl}
-          onChange={(event) => onChange({ ...formData, baseUrl: event.target.value })}
+          placeholder="application/json"
+          label={translator("assets.new.fieldDataAddressContentType")}
+          error={errors.contentType}
+          value={formData.contentType}
+          onChange={(event) => onChange({...formData, contentType: event.target.value})}
         />
-        <RadioButton
-          labelTrue={translator("assets.new.fieldDataAddressHttpProxyPathTrue")}
-          labelFalse={translator("assets.new.fieldDataAddressHttpProxyPathFalse")}
-          id="data-address-http-proxy-path"
-          value={formData.proxyPath}
-          onChange={(value) => onChange({ ...formData, proxyPath: value })}
+
+        <Input
+          name="data-address-proxy-body"
+          id="data-address-proxy-body"
+          key="data-address-proxy-body"
+          multiline
+          rows={3}
+          label={translator("assets.new.fieldDataAddressProxyBody")}
+          placeholder={'{"some": "request-body"}'}
+          required
+          classes={{ textField: { '& p':{ color: theme.palette.error.main } }} as any}
+          error={errors.proxyBody}
+          value={formData.proxyBody}
+          onChange={(event) => onChange({ ...formData, proxyBody: event.target.value })}
         />
-      </div>
+      </div>}
     </>
   );
 }
