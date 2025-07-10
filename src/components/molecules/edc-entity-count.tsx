a5fb8181bf4f3795ac7507a63fb32bce0ff5e78b
Moatez Mkhinini <moatezmkhinini@think-it.io>
2025-07-10 17:04:33 +0100

    feat: started doing dashboard test

--- src/components/molecules/edc-entity-count.tsx
+++ src/components/molecules/edc-entity-count.tsx
@@ -10,11 +10,12 @@ export interface EdcEntityCountProps {
   label: string;
   count: number;
   className?: string;
+  "data-testid"?: string;
 }
 
-export function EdcEntityCount({ label, count, className = "" }: EdcEntityCountProps): JSX.Element {
+export function EdcEntityCount({ label, count, className = "", "data-testid": dataTestId }: EdcEntityCountProps): JSX.Element {
   return (
-    <Card className={className} >
+    <Card className={className} data-testid={dataTestId} >
       <CardContent className="flex flex-col gap-y-4">
         <Typography className="!text-[81px] text-center" component="h2">
           {count}
