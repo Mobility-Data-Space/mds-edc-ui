454a97948c23d53c30f71c45a4b0f8b433757556
Ramy Hcini <ramyhcini@think-it.io>
2025-07-23 13:09:05 +0200

    feat: add env variables for curator, maintainer and daps and show them in dashboard

--- src/constants/dataspace.ts
+++ src/constants/dataspace.ts
@@ -7,79 +7,10 @@ export interface Participant {
   connectorManagementUrl: string;
   defaultUrl: string;
   protocolUrl: string;
+  curatorName: string,
+  curatorUrl: string,
+  maintainerName: string,
+  maintainerUrl: string,
+  dapsUrl: string
 }
 
-class Dataspace {
-  #inner: Map<string, Record<string, Participant[]>> = new Map();
-  constructor(
-    entries?:
-      | readonly (readonly [string, Record<string, Participant[]>])[]
-      | null,
-  ) {
-    this.#inner = new Map(entries);
-  }
-
-  findParticipant(
-    useCase: string,
-    environment: string,
-    participantId: string,
-  ): Participant | undefined {
-    return this.#inner.get(useCase)?.[environment]?.find(({ id }) =>
-      id === participantId
-    );
-  }
-
-  filter(
-    useCase: string,
-    environment: string,
-    fn: (item: Participant, index: number, array: Participant[]) => boolean,
-  ): Participant[] {
-    return this.#inner.get(useCase)?.[environment]?.filter(fn) ?? [];
-  }
-
-  map<T>(
-    useCase: string,
-    environment: string,
-    fn: (item: Participant, index: number, array: Participant[]) => T,
-  ): T[] {
-    return this.#inner.get(useCase)?.[environment]?.map(fn) ?? [];
-  }
-}
-
-export const DATASPACE = new Dataspace([
-  ["mobility-data-space", {
-    local: [
-      {
-        id: "mds-local",
-        name: "MDS UI (local)",
-        edcUrl: "localhost:3000",
-        managementUrl: "http://localhost:3000/api/3001/management",
-        connectorManagementUrl: "http://localhost:3000/api/3001/management",
-        defaultUrl: "http://localhost:3000/api/8860/api",
-        protocolUrl: "http://zf-circular-economy-integration:9194/protocol",
-      },
-    ],
-    dev: [
-      {
-        id: "mds-dev",
-        name: "MDS UI (dev)",
-        edcUrl: "localhost:3000",
-        managementUrl: "http://localhost:3000/api/3001/management",
-        connectorManagementUrl: "http://localhost:3000/api/3001/management",
-        defaultUrl: "http://localhost:3000/api/8860/api",
-        protocolUrl: "http://zf-circular-economy-integration:9194/protocol",
-      },
-    ],
-    prod: [
-      {
-        id: "mds-prod",
-        name: "MDS UI (prod)",
-        edcUrl: "localhost:3000",
-        managementUrl: "http://localhost:3000/api/3003/management",
-        connectorManagementUrl: "http://localhost:3000/api/3003/management",
-        defaultUrl: "http://localhost:3000/api/8870/api",
-        protocolUrl: "http://zf-circular-economy-production:9194/protocol",
-      },
-    ],
-  }]
-]);
