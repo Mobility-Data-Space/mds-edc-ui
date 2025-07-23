5a7b5785928fdf4e2960eccd3bc0c364327ea584
Ramy Hcini <ramyhcini@think-it.io>
2025-07-23 13:10:43 +0200

    refactor: global test setup

--- tests/utils/ui-config.ts
+++ tests/utils/ui-config.ts
@@ -6,5 +6,10 @@ export const config = {
   EDC_DEFAULT_URL: "http://127.0.0.1:8181/api",
   EDC_PROTOCOL_URL: "http://127.0.0.1:8183/api/dsp",
   EDC_PUBLIC_URL: "http://127.0.0.1:8185/public",
-  EDC_MANAGEMENT_API_KEY: ""
+  EDC_MANAGEMENT_API_KEY: "my-test-api-key",
+  EDC_CURATOR_ORGANIZATION: "Think-it GmbH",
+  EDC_CURATOR_URL: "https://think-it.io",
+  EDC_MAINTAINER_ORGANIZATION: "Think-it GmbH",
+  EDC_MAINTAINER_URL: "https://think-it.io",
+  MDS_DAPS_URL: "https://daps.mobility-dataspace.eu"
 }
