be4674d2a8944719156240d084420842824d8c1d
Ramy Hcini <ramyhcini@think-it.io>
2025-07-24 14:43:13 +0200

    refactor: test setup

--- tests/utils/ui-config.ts
+++ tests/utils/ui-config.ts
@@ -1,4 +1,4 @@
-export const config = {
+export const participantConfig = {
   EDC_ID: "MDSXXXXX.YYYYY",
   EDC_NAME: "My MDS EDC Connector",
   EDC_DESCRIPTION: "This is the connector description from env variables",
@@ -13,3 +13,20 @@ export const config = {
   EDC_MAINTAINER_URL: "https://think-it.io",
   MDS_DAPS_URL: "https://daps.mobility-dataspace.eu"
 }
+
+export const counterPartyParticipantConfig = {
+  EDC_ID: "MDSXXXXX.ZZZZ",
+  EDC_NAME: "My MDS EDC Connector",
+  EDC_DESCRIPTION: "This is the connector description from env variables",
+  EDC_MANAGEMENT_URL: "http://127.0.0.1:9182/api/management",
+  EDC_DEFAULT_URL: "http://127.0.0.1:9181/api",
+  EDC_PROTOCOL_URL: "http://127.0.0.1:9183/api/dsp",
+  EDC_PUBLIC_URL: "http://127.0.0.1:9185/public",
+  EDC_MANAGEMENT_API_KEY: "my-test-api-key",
+  EDC_CURATOR_ORGANIZATION: "Think-it GmbH",
+  EDC_CURATOR_URL: "https://think-it.io",
+  EDC_MAINTAINER_ORGANIZATION: "Think-it GmbH",
+  EDC_MAINTAINER_URL: "https://think-it.io",
+  MDS_DAPS_URL: "https://daps.mobility-dataspace.eu"
+}
+
