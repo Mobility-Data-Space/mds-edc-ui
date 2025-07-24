fac95b773dc12bbdde2936ef2ba23e89355d67a2
Ramy Hcini <ramyhcini@think-it.io>
2025-07-24 17:25:45 +0200

    refactor: use env vars for test API Keys

--- tests/utils/tests-config.ts
+++ tests/utils/tests-config.ts
@@ -6,7 +6,7 @@ export const participantConfig = {
   EDC_DEFAULT_URL: "http://localhost:8181/api",
   EDC_PROTOCOL_URL: "http://localhost:8183/api/dsp",
   EDC_PUBLIC_URL: "http://localhost:8185/public",
-  EDC_MANAGEMENT_API_KEY: "my-test-api-key",
+  EDC_MANAGEMENT_API_KEY: process.env.TEST_API_KEY || "default-test-api-key",
   EDC_CURATOR_ORGANIZATION: "Think-it GmbH",
   EDC_CURATOR_URL: "https://think-it.io",
   EDC_MAINTAINER_ORGANIZATION: "Think-it GmbH",
@@ -22,11 +22,10 @@ export const counterPartyParticipantConfig = {
   EDC_DEFAULT_URL: "http://localhost:9181/api",
   EDC_PROTOCOL_URL: "http://localhost:9183/api/dsp",
   EDC_PUBLIC_URL: "http://localhost:9185/public",
-  EDC_MANAGEMENT_API_KEY: "my-test-api-key",
+  EDC_MANAGEMENT_API_KEY: process.env.TEST_API_KEY || "default-test-api-key",
   EDC_CURATOR_ORGANIZATION: "Think-it GmbH",
   EDC_CURATOR_URL: "https://think-it.io",
   EDC_MAINTAINER_ORGANIZATION: "Think-it GmbH",
   EDC_MAINTAINER_URL: "https://think-it.io",
   MDS_DAPS_URL: "https://daps.mobility-dataspace.eu"
 }
-
