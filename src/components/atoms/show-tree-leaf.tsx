a0366a0bb0c5309f38388d697467b1ea840afc7e
Moatez Mkhinini <moatezmkhinini@think-it.io>
2025-07-30 02:15:22 +0100

    fix: asset name truncation

--- src/components/atoms/show-tree-leaf.tsx
+++ src/components/atoms/show-tree-leaf.tsx
@@ -5,7 +5,7 @@ export function ShowTreeLeaf({hidden, disablePadding = false, children}: PropsWi
   return (
     <div className={`${disablePadding ? "" : "py-2"} relative pl-0 w-full flex`}>
       {hidden ? "" : <span
-        className={`mt-1 mb-0 mr-2.5 h-5 size-3 inline-block border-b-2 border-black`}
+        className={`mt-1 mb-0 mr-2.5 h-5 size-3 min-w-2.5 inline-block border-b-2 border-black`}
       />}
       <div className="flex-grow justify-center flex flex-col">
         {children}
--- src/components/molecules/constraint-show.tsx
+++ src/components/molecules/constraint-show.tsx
@@ -41,57 +41,57 @@ function constraintTooltipAndValue(value: string, index: number, translator: (ke
 
 export function ConstraintShow({ data }: ConstraintShowProps): ReactNode {
   const { translator } = useTranslator();
-    if (typeof data === 'string') {
-      return (
-        <div className="flex gap-x-2 items-center">{
-          data.split(",").map((value, index) => {
-            const [tooltipTitle, computedValue] = constraintTooltipAndValue(value, index, translator);
-            return (
-              <Tooltip title={tooltipTitle} key={index}>
-                <Typography component="span">
-                  {computedValue}
-                </Typography>
-              </Tooltip>
-            );
-          })
-        }</div>
-      );
-    }
+  if (typeof data === 'string') {
+    return (
+      <div className="flex gap-x-2 items-center">{
+        data.split(",").map((value, index) => {
+          const [tooltipTitle, computedValue] = constraintTooltipAndValue(value, index, translator);
+          return (
+            <Tooltip title={tooltipTitle} key={index}>
+              <Typography component="span" className={index > 1 ? "[word-break:break-word]" : ""}>
+                {computedValue}
+              </Typography>
+            </Tooltip>
+          );
+        })
+      }</div>
+    );
+  }
 
-    if (Array.isArray(data)) {
-      const lastIndex = data.length - 1;
-      return data.map((item, index) => (
-        <ShowTreeLeaf disablePadding key={index} >
-          <div className="pt-2">
-            <ConstraintShow data={item} />
-            {lastIndex !== index ? "" : <div className="bg-white absolute -left-1 bottom-0 size-2" />}
-          </div>
-        </ShowTreeLeaf>
-      ));
-    }
-
-    if (typeof data !== 'object' || !data) {
-      return [null, undefined].indexOf(data) === -1 ? String(data) : "";
-    }
-
-    let html = [];
-    for (const key in data) {
-      const [tooltip] = tryTranslatingWithTooltip(key, "policyDefinitions.constraint", translator)
-      html.push(
-        <div key={key}>
-          <Tooltip title={tooltip}>
-            <Typography component="span" className="p-3 py-1 inline-block uppercase">
-              {key}
-            </Typography>
-          </Tooltip>
-          <div className="">
-            <ShowTreeBranch bottomLeafHidden>
-              <ConstraintShow data={data[key]} />
-            </ShowTreeBranch>
-          </div>
+  if (Array.isArray(data)) {
+    const lastIndex = data.length - 1;
+    return data.map((item, index) => (
+      <ShowTreeLeaf disablePadding key={index} >
+        <div className="pt-2">
+          <ConstraintShow data={item} />
+          {lastIndex !== index ? "" : <div className="bg-white absolute -left-1 bottom-0 size-2" />}
         </div>
-      );
-    }
+      </ShowTreeLeaf>
+    ));
+  }
 
-    return html;
+  if (typeof data !== 'object' || !data) {
+    return [null, undefined].indexOf(data) === -1 ? String(data) : "";
+  }
+
+  let html = [];
+  for (const key in data) {
+    const [tooltip] = tryTranslatingWithTooltip(key, "policyDefinitions.constraint", translator)
+    html.push(
+      <div key={key}>
+        <Tooltip title={tooltip}>
+          <Typography component="span" className="p-3 py-1 inline-block uppercase">
+            {key}
+          </Typography>
+        </Tooltip>
+        <div>
+          <ShowTreeBranch bottomLeafHidden>
+            <ConstraintShow data={data[key]} />
+          </ShowTreeBranch>
+        </div>
+      </div>
+    );
+  }
+
+  return html;
 }
