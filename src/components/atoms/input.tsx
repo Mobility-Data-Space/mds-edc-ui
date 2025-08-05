1557ee539997d8c949a756d4822350583b90fd11
Mohamed Dhia <mohameddhia365@gmail.com>
2025-08-05 02:36:03 +0100

    feat: lift search state to application level

--- src/components/atoms/input.tsx
+++ src/components/atoms/input.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import {TextField, Tooltip, IconButton} from "@mui/material";
+import React, { ForwardedRef, forwardRef } from "react";
+import { TextField, Tooltip, IconButton } from "@mui/material";
 import { TextFieldProps } from "@mui/material/TextField";
 import { InfoOutlined } from "@mui/icons-material";
 
-export function Input({ tooltip = "", onChange, value, error, ...rest }: Omit<TextFieldProps, "error"> & { tooltip?: string, error?: string | boolean }): JSX.Element {
+export const Input = forwardRef(({ tooltip = "", onChange, value, error, ...rest }: Omit<TextFieldProps, "error"> & { tooltip?: string, error?: string | boolean }, ref: ForwardedRef<HTMLInputElement>): JSX.Element => {
 
   return (
     <TextField
@@ -14,15 +14,16 @@ export function Input({ tooltip = "", onChange, value, error, ...rest }: Omit<Te
       placeholder={rest.placeholder}
       helperText={typeof error === "string" ? error : ""}
       {...rest}
-      error={!! error}
+      error={!!error}
       slotProps={{
         input: {
-          endAdornment: ! tooltip ? "" : <Tooltip title={tooltip}><IconButton><InfoOutlined /></IconButton></Tooltip>
+          endAdornment: !tooltip ? "" : <Tooltip title={tooltip}><IconButton><InfoOutlined /></IconButton></Tooltip>
         },
         ...rest.slotProps
       }}
       value={value === null || value === undefined ? "" : value}
       onChange={onChange}
+      inputRef={ref}
     />
   );
-}
+})
--- src/components/molecules/search-bar.tsx
+++ src/components/molecules/search-bar.tsx
@@ -1,57 +1,83 @@
 import { T } from "@/i18n";
-import {Input} from "@/components/atoms/input";
-import {List, useListContext} from "../../../vendors/think-it-labs/edc-connector-ui/src/list";
+import { Input } from "@/components/atoms/input";
+import { List, useListContext } from "../../../vendors/think-it-labs/edc-connector-ui/src/list";
 import { SearchSpec } from "../../../vendors/think-it-labs/edc-connector-ui/src/types";
-import {Button, Icon, IconButton, Tooltip} from "@mui/material";
-import React, {useEffect} from "react";
-import {InfoOutlined} from "@mui/icons-material";
+import { Button, Icon } from "@mui/material";
+import React, { useCallback, useEffect, useRef } from "react";
+import { useRouter } from "next/router";
 
 interface SearchBarProps {
-    placeholder: string;
-    searchTarget: string;
-    searchOperator: SearchSpec["operator"]
+  placeholder: string;
+  searchTarget: string;
+  searchOperator: SearchSpec["operator"]
 }
 
 export default function SearchBar({ placeholder, searchTarget, searchOperator }: SearchBarProps) {
+  const { query, push } = useRouter()
+
   const { searchSpec, setSearchSpec, triggerSearch } = useListContext();
 
+  const searchRef = useRef<HTMLInputElement>(null)
+
+  const searchQuery = query.q
+
   useEffect(() => {
-    setSearchSpec({ operator: searchOperator, operandLeft: searchTarget })
+    setSearchSpec({ operator: searchOperator, operandLeft: searchTarget, })
   }, [setSearchSpec, searchTarget, searchOperator])
 
-    return (
-      <div className="relative flex rounded-lg h-full" >
-        <Input
-          className="!pr-0 rounded"
-          placeholder={placeholder}
-          value={searchSpec.operandRight}
-          onChange={(event) => setSearchSpec({operandRight: event.currentTarget.value})}
-          onKeyDown={(event) => {
-            if (event.key === "Enter") {
-              triggerSearch();
-            }
-          }}
-          slotProps={{
-            input: {
-              className: "!pr-0 h-full",
-              startAdornment: <Icon className="size-5">search</Icon>,
-              endAdornment: <Button
-                data-testid="search-trigger"
-                variant="contained"
-                className="gap-x-2 font-medium h-full hover:cursor-pointer"
-                style={{
-                  borderTopRightRadius: 4,
-                  borderBottomRightRadius: 4,
-                  boxShadow: "0px 0px 1px -2px rgba(0,0,0,0.2),0px 0px 2px 0px rgba(0,0,0,0.14),0px 0px 5px 0px rgba(0,0,0,0.12)",
-                }}
+  useEffect(() => {
+    setSearchSpec({ operandRight: searchQuery })
+  }, [searchQuery])
+
+  const handleSearch = useCallback(() => { // TODO: use useUpdateQueryParams when merged
+    if (searchRef.current) {
+      push({
+        href: window.location.href,
+        query: {
+          ...query,
+          q: searchRef.current.value,
+          page: 0
+        }
+      })
+    }
+  }, [])
+
+  return (
+    <div className="relative flex rounded-lg h-full" >
+      <Input
+        className="!pr-0 rounded"
+        ref={searchRef}
+        placeholder={placeholder}
+        value={searchSpec.operandRight}
+        onChange={(event) => setSearchSpec({ operandRight: event.currentTarget.value })}
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            handleSearch()
+          }
+        }}
+        slotProps={{
+          input: {
+            className: "!pr-0 h-full",
+            startAdornment: <Icon className="size-5">search</Icon>,
+            endAdornment: <Button
+              data-testid="search-trigger"
+              variant="contained"
+              className="gap-x-2 font-medium h-full hover:cursor-pointer"
+              style={{
+                borderTopRightRadius: 4,
+                borderBottomRightRadius: 4,
+                boxShadow: "0px 0px 1px -2px rgba(0,0,0,0.2),0px 0px 2px 0px rgba(0,0,0,0.14),0px 0px 5px 0px rgba(0,0,0,0.12)",
+              }}
+            >
+              <span
+                onClick={() => handleSearch()}
               >
-                <List.SearchTrigger>
-                  <T global string="search"/>
-                </List.SearchTrigger>
-              </Button>
-            },
-          }}
-        />
-      </div>
-    );
+                <T global string="search" />
+              </span>
+            </Button>
+          },
+        }}
+      />
+    </div>
+  );
 }
