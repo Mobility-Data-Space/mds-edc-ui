91df97de689b3f306e2da9b98b36f3ef8d249a17
Ramy Hcini <ramyhcini@think-it.io>
2025-08-07 00:26:21 +0200

    feat: add placeholder for consumer id field and helper text

--- src/components/molecules/consumer-participant-id-constraint.tsx
+++ src/components/molecules/consumer-participant-id-constraint.tsx
@@ -1,25 +1,41 @@
-import {Icon, IconButton} from "@mui/material";
+import {FormHelperText, Icon, IconButton} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { AtomicConstraint } from "@think-it-labs/edc-connector-client";
 import { Input } from "@/components/atoms/input";
 import { MuiSelect } from "@/components/atoms/mui-select";
 import { ConstraintProps } from "@/components/molecules/constraint";
-import { T } from "@/i18n";
+import { T, useTranslator } from "@/i18n";
 import { consumerParticipantIdOperators } from "@/utilities/policy-operators";
 import React from "react";
 
 export function ConsumerParticipantIdConstraint({ value, onChange, onRemove }: ConstraintProps) {
   value = value as AtomicConstraint;
+  const { translator } = useTranslator() ;
 
   return (
-    <div className="flex flex-row gap-4">
+    <div className="flex flex-row gap-5">
+      <div className="flex flex-col">
       <Typography variant="body2">
         <T string="dataOffer.new.policyExpressionConsumerParticipantId" />
       </Typography>
-      <MuiSelect label="participant-id" options={consumerParticipantIdOperators} value={value.operator}
-        onChange={(event) => onChange({ ...value, operator: event.target.value })} />
-      <Input data-testid="participant-id-field" error={!value.rightOperand} value={value.rightOperand} onChange={(event) => onChange({ ...value, rightOperand: event.target.value })} />
-      <div className="flex items-center">
+      </div>
+      <div className="flex flex-col">
+        <MuiSelect 
+          label={translator("dataOffer.new.policyExpressionOperator")}
+          options={consumerParticipantIdOperators} value={value.operator}
+          onChange={(event) => onChange({ ...value, operator: event.target.value })} />
+      </div>
+      <div className="flex flex-col">
+        <Input 
+          data-testid="participant-id-field"
+          placeholder={translator("dataOffer.new.policyExpressionConsumerParticipantIdExamples")}
+          error={!value.rightOperand} value={value.rightOperand} 
+          onChange={(event) => onChange({ ...value, rightOperand: event.target.value })} />
+        <FormHelperText className="flex flex-row">
+          <T string="dataOffer.new.policyExpressionConsumerParticipantIdTooltip"/>
+        </FormHelperText>
+      </div>
+      <div className="flex">
         <IconButton
           size="large"
           onClick={onRemove}
