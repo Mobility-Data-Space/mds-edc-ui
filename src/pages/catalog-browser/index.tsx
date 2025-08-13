cc307ebb149e3737e632a77078e916f6bf0bf88b
Mohamed Dhia <mohameddhia365@gmail.com>
2025-08-13 05:04:28 +0100

    fix: loader position

--- src/pages/catalog-browser/index.tsx
+++ src/pages/catalog-browser/index.tsx
@@ -192,19 +192,19 @@ export default function CatalogPage() {
                 </Typography>
 
               </div>}
+              <ContractOffersList.Loading>
+                <div className="max-w-20 mx-auto mt-4 flex flex-col bg-white border shadow-sm rounded-xl p-4 md:p-5 self-start">
+                  <span
+                    className="animate-spin mx-auto inline-block size-8 border-[3px] border-current border-t-transparent text-blue-600 rounded-full"
+                    role="status"
+                    aria-label="loading"
+                  >
+                    <span className="sr-only">Loading...</span>
+                  </span>
+                </div>
+              </ContractOffersList.Loading>
             </div>
 
-            <ContractOffersList.Loading>
-              <div className="max-w-20 mx-auto mt-4 flex flex-col bg-white border shadow-sm rounded-xl p-4 md:p-5">
-                <span
-                  className="animate-spin mx-auto inline-block size-8 border-[3px] border-current border-t-transparent text-blue-600 rounded-full"
-                  role="status"
-                  aria-label="loading"
-                >
-                  <span className="sr-only">Loading...</span>
-                </span>
-              </div>
-            </ContractOffersList.Loading>
             <ContractOffersList.Error>
               {({ errors }) => (
                 <ErrorPopup
