a39452b92073ce07e2e9751b53d1f1945eb3e019
Mohamed Dhia <56113399+mohamed-Dhia@users.noreply.github.com>
2025-09-11 12:54:59 +0100

    fix: terminated chip text color (#299)
    * fix: terminated chip text color

--- src/constants/transfer-process.ts
+++ src/constants/transfer-process.ts
@@ -4,20 +4,32 @@ export const STATE_RUNNING = "RUNNING";
 export const STATE_ERROR = "ERROR";
 export const STATE_FINALIZED = "FINALIZED";
 
-export const COLORS: { [key: string]: string } = {
+export const TEXT_COLORS: { [key: string]: string } = {
+  [STATE_RUNNING]: "black",
+  [TransferProcessStates.STARTED]: "black",
+  [TransferProcessStates.DEPROVISIONED]: "black",
+  [STATE_ERROR]: "black",
+  [TransferProcessStates.TERMINATED]: "white",
+  [STATE_FINALIZED]: "black",
+  [TransferProcessStates.COMPLETED]: "black",
+};
+
+export const BG_COLORS: { [key: string]: string } = {
   [STATE_RUNNING]: "#FFFF00",
   [TransferProcessStates.STARTED]: "#FFFF00",
+  [TransferProcessStates.COMPLETED]: "#96D200",
   [TransferProcessStates.DEPROVISIONED]: "#6E7378",
   [STATE_ERROR]: "#B91C1C",
   [TransferProcessStates.TERMINATED]: "#000000",
   [STATE_FINALIZED]: "#96D200",
 };
 
-export const HOVER_COLORS: { [key: string]: string } = {
+export const BG_HOVER_COLORS: { [key: string]: string } = {
   [STATE_RUNNING]: "#E0E000",
   [TransferProcessStates.STARTED]: "#E0E000",
   [TransferProcessStates.DEPROVISIONED]: "#C8CACC",
   [STATE_ERROR]: "#A11818",
   [TransferProcessStates.TERMINATED]: "#000000",
+  [TransferProcessStates.COMPLETED]: "#96D200",
   [STATE_FINALIZED]: "#87BD00",
 };
