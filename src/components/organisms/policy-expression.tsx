c39674bb0c56bd404ad7a8803c7638cee7fd2b38
Mohamed Dhia <mohameddhia365@gmail.com>
2025-10-07 10:46:48 +0100

    uppercase policy restriction operators and fix double leaf

--- src/components/organisms/policy-expression.tsx
+++ src/components/organisms/policy-expression.tsx
@@ -3,7 +3,7 @@ import { TreeLeaf } from "@/components/atoms/tree-leaf";
 import { Constraint } from "@/components/molecules/constraint";
 import { AddConstraintButton } from "@/components/organisms/add-constraint-button";
 import { MultiplicityConstraint } from "@/utilities/policy-constraints";
-import {Icon, IconButton} from "@mui/material";
+import { Icon, IconButton } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { AtomicConstraint } from "@think-it-labs/edc-connector-client";
 import { ReactNode } from "react";
@@ -18,29 +18,42 @@ export interface PolicyExpressionProps {
   showAddButton?: boolean;
 }
 
-export default function PolicyExpression({ value, onChange, onRemove, title = "", isFirstLevel = true, showAddButton = false }: PolicyExpressionProps) {
-  const resolvedShowAddButton = showAddButton || (isFirstLevel && value.length === 0);
+export default function PolicyExpression({
+  value,
+  onChange,
+  onRemove,
+  title = "",
+  isFirstLevel = true,
+  showAddButton = false,
+}: PolicyExpressionProps) {
+  const resolvedShowAddButton =
+    showAddButton || (isFirstLevel && value.length === 0);
   const hideVerticalAndHorizontalLine = isFirstLevel && value.length <= 1;
 
-  const createOnChange = (index: number) => (newConstraint: AtomicConstraint | MultiplicityConstraint) => {
-    const result = [...value];
-    result[index] = newConstraint;
-    return onChange(result);
-  };
+  const createOnChange =
+    (index: number) =>
+    (newConstraint: AtomicConstraint | MultiplicityConstraint) => {
+      const result = [...value];
+      result[index] = newConstraint;
+      return onChange(result);
+    };
 
   const createOnRemove = (index: number) => () => {
     const result = [...value];
-    result.splice(index, 1)
+    result.splice(index, 1);
     return onChange(result);
   };
 
-  const onAdd = (newConstraint: AtomicConstraint | MultiplicityConstraint) => onChange([...value, newConstraint]);
+  const onAdd = (newConstraint: AtomicConstraint | MultiplicityConstraint) =>
+    onChange([...value, newConstraint]);
 
   return (
     <div>
-      {!title ? "" :
+      {!title ? (
+        ""
+      ) : (
         <div className="pb-4">
-          <Typography component="span" className="p-3 inline-block">
+          <Typography component="span" className="p-3 inline-block uppercase">
             {title}
           </Typography>
 
@@ -50,28 +63,38 @@ export default function PolicyExpression({ value, onChange, onRemove, title = ""
             className="gap-x-2 font-medium float-right"
             color="secondary"
           >
-            <Icon data-testid="add-expression-button" style={{ fontSize: "28px" }} >remove</Icon>
+            <Icon
+              data-testid="add-expression-button"
+              style={{ fontSize: "28px" }}
+            >
+              remove
+            </Icon>
           </IconButton>
         </div>
-      }
+      )}
       <TreeBranch hidden={hideVerticalAndHorizontalLine}>
-        {!value || value.length === 0 ? "" :
-          value.map((constraint, index) =>
-            <TreeLeaf key={index} hidden={hideVerticalAndHorizontalLine}>
-              <Constraint
-                value={constraint}
-                onChange={createOnChange(index)}
-                onRemove={createOnRemove(index)}
-              />
-            </TreeLeaf>
-          )
-        }
+        {!value || value.length === 0
+          ? ""
+          : value.map((constraint, index) => (
+              <TreeLeaf key={index} hidden={hideVerticalAndHorizontalLine}>
+                <Constraint
+                  value={constraint}
+                  onChange={createOnChange(index)}
+                  onRemove={createOnRemove(index)}
+                />
+              </TreeLeaf>
+            ))}
 
-        {!resolvedShowAddButton ? "" :
-          <TreeLeaf hidden={hideVerticalAndHorizontalLine}>
-            <AddConstraintButton onClick={onAdd} showAddButton={resolvedShowAddButton} />
+        {!resolvedShowAddButton ? (
+          ""
+        ) : (
+          <TreeLeaf hidden>
+            <AddConstraintButton
+              onClick={onAdd}
+              showAddButton={resolvedShowAddButton}
+            />
           </TreeLeaf>
-        }
+        )}
       </TreeBranch>
     </div>
   );
