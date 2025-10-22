73b77e5c6a15b32886eb8c9baad7a921262e47fe
Mohamed Dhia <mohameddhia365@gmail.com>
2025-10-22 09:48:44 +0100

    fix: fetch only today ids

--- src/utilities/contract-definition.ts
+++ src/utilities/contract-definition.ts
@@ -1,25 +1,32 @@
-import {removeEmptyFields} from "@/utilities/form";
+import { removeEmptyFields } from "@/utilities/form";
 import { ContractDefinitionInput } from "@think-it-labs/edc-connector-client";
 import { generateDataOfferId } from "@/utilities/data-offer";
 
-export type MdsContractDefinitionInput = ContractDefinitionInput & { privateProperties: { manualApproval: boolean } };
+export type MdsContractDefinitionInput = ContractDefinitionInput & {
+  privateProperties: { manualApproval: boolean };
+};
 
-export const fromContractDefinitionForm = (formData: MdsContractDefinitionInput): MdsContractDefinitionInput => {
+export const fromContractDefinitionForm = (
+  formData: MdsContractDefinitionInput,
+): MdsContractDefinitionInput => {
   const cleanFormDataObject = removeEmptyFields(formData);
-  return cleanFormDataObject as MdsContractDefinitionInput
+  return cleanFormDataObject as MdsContractDefinitionInput;
 };
 
-export const defaultCreateContractDefinitionFormData: MdsContractDefinitionInput = {
-  "@id": "", 
-  accessPolicyId: "",
-  contractPolicyId: "",
-  assetsSelector: [],
-  privateProperties: {
-    manualApproval: false
-  }
-};
+export const defaultCreateContractDefinitionFormData: MdsContractDefinitionInput =
+  {
+    "@id": "",
+    accessPolicyId: "",
+    contractPolicyId: "",
+    assetsSelector: [],
+    privateProperties: {
+      manualApproval: false,
+    },
+  };
 
-export const createDefaultContractDefinitionFormData = (existingIds: string[] = []): MdsContractDefinitionInput => ({
+export const createDefaultContractDefinitionFormData = (
+  id: string,
+): MdsContractDefinitionInput => ({
   ...defaultCreateContractDefinitionFormData,
-  "@id": generateDataOfferId(existingIds)
+  "@id": id,
 });
