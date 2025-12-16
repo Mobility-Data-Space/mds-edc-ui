08e8b633576d5d9ad9d73f23d37c7f939fe501c7
chidioguejiofor <chidioguejiofor@gmail.com>
2025-12-16 08:30:31 +0100

    fix: disable button when policy restricted expression is empty

--- src/pages/create-data-offer/index.tsx
+++ src/pages/create-data-offer/index.tsx
@@ -179,6 +179,8 @@ export default function CreateDataOfferPage() {
         return false;
       }
 
+      if(policyExpression.length === 0) return true;
+
       return policyExpression.some((policy): boolean => {
         if (isAtomicConstraint(policy)) {
           return !policy.rightOperand;
